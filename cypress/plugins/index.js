618d4fad072bd0e7da4eea8c5fddc07f5892c31f
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2022-09-30 21:45:48 +0500

    update plugins/index.js, support/index.js for wring-cypress-plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,17 +11,6 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-
-const { addScreenshotHook } = require(
-  '@aichemy/testgold-cypress-plugin/src/tgScreenshotHook'
-)
-
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  addScreenshotHook(on, config)
+module.exports = function (on, config) {
+  // configure plugins here
 }
